6b232675243b70a79dc46c67e42b6d481b8e5714
pedrogussi <pedrogussi@hotmail.com>
2021-02-11 17:52:57 -0300

    post-edeting

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 
@@ -26,4 +27,16 @@ export const createPost = async (req, res) => {
         res.status(409).json({message: error.message})
 
     }
+}
+
+export const updatePost = async (req, res) => {
+    const {id: _id} =req.params;
+
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post found');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+
+    res.json(updatedPost);
 }
